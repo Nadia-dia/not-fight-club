616b2e62ddedab22d50eefa4aa3272440eb17b12
Nadzeya Kaliada <hopenadya88@gmail.com>
2025-08-17 18:46:45 +0200

    feat: add on homepage html&css, events

--- app.js
+++ app.js
@@ -16,3 +16,22 @@ function validateForm(event){
 
 const form=document.querySelector(".register-form");
 form.addEventListener("submit", validateForm);
+
+// Nav buttons pressed -- fix!
+document.querySelectorAll(".item__link").forEach(link => {
+  link.addEventListener("click", event => {
+    event.preventDefault(); 
+    
+    //prevent reload if already on that page
+    let current = window.location.pathname.split("/").pop();
+    console.log(current);
+    if (current === "") current = "index.html"; // default
+    const target=link.getAttribute("href");
+
+    if(current !== target){
+      window.location.href=target;       
+    } else{
+      link.classList.add("active"); 
+    };
+  });
+})
