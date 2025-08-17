abe7798e07818b1988ff6519430c8c0c25c9adb8
Nadzeya Kaliada <hopenadya88@gmail.com>
2025-08-17 19:10:16 +0200

    fix: prevent oage reloads and JS errors for non- existing elements

--- app.js
+++ app.js
@@ -11,27 +11,41 @@ function validateForm(event){
   } else {
     localStorage.setItem("username", username);
     window.location.href = "index.html";
-  }
-}
+  };
+};
 
 const form=document.querySelector(".register-form");
-form.addEventListener("submit", validateForm);
+if(form){form.addEventListener("submit", validateForm);};
 
 // Nav buttons pressed -- fix!
-document.querySelectorAll(".item__link").forEach(link => {
-  link.addEventListener("click", event => {
-    event.preventDefault(); 
+const links = document.querySelectorAll(".item__link")
+links.forEach(link => {
+  if(link){
+    link.addEventListener("click", event => {
+      event.preventDefault(); 
     
-    //prevent reload if already on that page
-    let current = window.location.pathname.split("/").pop();
-    console.log(current);
-    if (current === "") current = "index.html"; // default
-    const target=link.getAttribute("href");
+      //prevent reload if already on that page
+      let current = window.location.pathname.split("/").pop();
+      console.log(current);
+      if (current === "") current = "index.html"; // default
+      const target=link.getAttribute("href");
 
-    if(current !== target){
-      window.location.href=target;       
-    } else{
-      link.classList.add("active"); 
-    };
+      if(current !== target){
+        window.location.href=target;       
+      } else{
+        link.classList.add("active"); 
+      };
+    });
+  }
+});
+
+// Fight button pressed
+const fightButton = document.querySelector(".fight-button");
+console.log(fightButton);
+
+if(fightButton){
+  fightButton.addEventListener("click", event => {
+    event.preventDefault(); 
+    window.location.href = "fight.html";
   });
-})
+};
